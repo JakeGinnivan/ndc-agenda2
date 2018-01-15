12bb22e1522a1f7e670ccc87e14a324853b7d894
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2018-01-15 17:01:14 +0000

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
 import React, { Component } from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Route, Switch } from "react-router";
+import { Agenda } from "./pages/agenda";
+import { Home } from "./pages/home";
+
 import "./App.css";
-import { fetchAgenda } from "./fetch-agenda";
-import { groupBy } from "./utils";
-import { Day } from "./components/day";
-import { Tabs } from "./components/tabs";
 
 class App extends Component {
-    state = {
-        talks: []
-    };
-
-    async componentWillMount() {
-        const talks = await fetchAgenda();
-        this.setState({ talks });
-    }
-
     render() {
-        const days = groupBy(
-            this.state.talks,
-            talk => talk.day
-        );
-
         return (
-            <div className="App">
-                <Tabs
-                    tabs={days.map(day => ({
-                        header: `Day ${day.key}`,
-                        data: day
-                    }))}
-                    renderTab={data => (
-                        <Day
-                            day={data.key}
-                            talks={data.values}
+            <BrowserRouter>
+                <div className="App">
+                    <div>
+                        <Link to="/">Home</Link>
+                        <Link to="/agenda">Agenda</Link>
+                    </div>
+                    <Switch>
+                        <Route
+                            exact
+                            path="/"
+                            component={Home}
                         />
-                    )}
-                />
-            </div>
+                        <Route
+                            path="/agenda"
+                            component={Agenda}
+                        />
+                    </Switch>
+                </div>
+            </BrowserRouter>
         );
     }
 }
---
+++ src/pages/agenda.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { fetchAgenda } from "../fetch-agenda";
+import { groupBy } from "../utils";
+import { Day } from "../components/day";
+import { Tabs } from "../components/tabs";
+
+export class Agenda extends React.Component {
+    state = {
+        talks: []
+    };
+
+    async componentWillMount() {
+        const talks = await fetchAgenda();
+        this.setState({ talks });
+    }
+
+    render() {
+        const days = groupBy(
+            this.state.talks,
+            talk => talk.day
+        );
+
+        return (
+            <Tabs
+                tabs={days.map(day => ({
+                    header: `Day ${day.key}`,
+                    data: day
+                }))}
+                renderTab={data => (
+                    <Day
+                        day={data.key}
+                        talks={data.values}
+                    />
+                )}
+            />
+        );
+    }
+}
